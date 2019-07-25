738d3aa48d05bf25c64f79df442c1f71d8ce2af5
Azharuddin Ansari <azharuddinansari232@gmail.com>
2019-07-25 13:59:14 +0530

    Add files via upload

---
+++ BinarySearch.py
@@ -0,0 +1,11 @@
+def BinarySearch(arr,l,r,x):
+    if r >= l:
+        mid = l + (r-l)//2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return BinarySearch(arr,l,mid-1,x)
+        else:
+            return BinarySearch(arr,mid+1,r,x)
+    else:
+        return -1
---
+++ is_even.py
@@ -0,0 +1,8 @@
+def is_even(n):
+	if(n!=0):		
+		if(n%2==0):
+			return True
+		else:
+			return False
+	else:
+		return "Number is 0 which is not defined"
