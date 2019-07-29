655b3d4346cfcb1606d39a6f846aee935102271e
Azharuddin Ansari <azharuddinansari232@gmail.com>
2019-07-29 12:26:25 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,9 @@
+import sys
+#FUNCTION FOR FINDING FACTORIAL OF NUMBER N
 def fact(n):
     fact = 1
     for i in range(n,1,-1):
         fact *= i
     return fact
+#DRIVER CODE TO RUN FROM COMMAND LINE
+print(fact(input(sys.argv[1])))
